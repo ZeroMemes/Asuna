205e5152fbdf7cbdf46263a8729309a1527ebc88
EmotionalLove <totsuka.sama@gmail.com>
2018-12-07 09:05:34 -0800

    Move packetsend mixin so that it (hopefully) doesn't conflict

--- src/main/java/com/sasha/asuna/mod/mixin/client/MixinNetworkManager.java
+++ src/main/java/com/sasha/asuna/mod/mixin/client/MixinNetworkManager.java
@@ -26,10 +26,13 @@ import io.netty.util.concurrent.GenericFutureListener;
 import net.minecraft.network.INetHandler;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
+import org.checkerframework.checker.units.qual.A;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import javax.annotation.Nullable;
 
@@ -42,6 +45,15 @@ public abstract class MixinNetworkManager {
     @Shadow
     protected abstract void dispatchPacket(Packet<?> inPacket, @Nullable GenericFutureListener<? extends Future<? super Void>>[] futureListeners);
 
+    @Inject(method = "dispatchPacket", at = @At("HEAD"), cancellable = true)
+    public void dispatchPacket(final Packet<?> inPacket, @Nullable final GenericFutureListener<? extends Future<? super Void>>[] futureListeners, CallbackInfo info) {
+        ClientPacketSendEvent event = new ClientPacketSendEvent(inPacket);
+        AsunaMod.EVENT_MANAGER.invokeEvent(event);
+        if (event.isCancelled()) {
+            info.cancel();
+        }
+    }
+/*
     @Redirect(
             method = "sendPacket(Lnet/minecraft/network/Packet;)V",
             at = @At(
@@ -49,7 +61,7 @@ public abstract class MixinNetworkManager {
                     target = "net/minecraft/network/NetworkManager.dispatchPacket(Lnet/minecraft/network/Packet;[Lio/netty/util/concurrent/GenericFutureListener;)V"
             )
     )
-    private void onDispatchPacket(NetworkManager networkManager, Packet<?> packet, GenericFutureListener<? extends Future<?super Void>>[] futureListeners) {
+    private void onDispatchPacket(NetworkManager networkManager, Packet<?> packet, GenericFutureListener<? extends Future<? super Void>>[] futureListeners) {
         ClientPacketSendEvent event = new ClientPacketSendEvent(packet);
         AsunaMod.EVENT_MANAGER.invokeEvent(event);
         if (event.isCancelled()) {
@@ -57,7 +69,7 @@ public abstract class MixinNetworkManager {
         }
         this.dispatchPacket(event.getSendPacket(), futureListeners);
     }
-
+*/
     @Redirect(
             method = "channelRead0",
             at = @At(
