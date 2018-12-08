7440cf0106009fe96cf671a09b0975689a3e722e
EmotionalLove <totsuka.sama@gmail.com>
2018-12-08 15:49:16 -0800

    its still broken smh

--- src/main/java/com/sasha/asuna/mod/mixin/client/MixinNetworkManager.java
+++ src/main/java/com/sasha/asuna/mod/mixin/client/MixinNetworkManager.java
@@ -27,15 +27,16 @@ import net.minecraft.network.Packet;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * Created by Sasha on 08/08/2018 at 8:08 PM
  **/
-@Mixin(value = NetworkManager.class, priority = 999)
+@Mixin(value = NetworkManager.class, priority = 1001)
 public abstract class MixinNetworkManager {
 
-    @Inject(method = "sendPacket(Lnet/minecraft/network/Packet;)V", at = @At(value = "INVOKE",  target = "Lnet/minecraft/network/NetworkManager;flushOutboundQueue()V"), cancellable = true)
+    @Inject(method = "sendPacket(Lnet/minecraft/network/Packet;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkManager;flushOutboundQueue()V"), cancellable = true)
     public void sendPacket(Packet<?> packetIn, CallbackInfo info) {
         ClientPacketSendEvent event = new ClientPacketSendEvent(packetIn);
         AsunaMod.EVENT_MANAGER.invokeEvent(event);
@@ -46,16 +47,19 @@ public abstract class MixinNetworkManager {
 
     @Inject(
             method = "channelRead0",
-                    at = @At(
+            slice = @Slice(
+                    from = @At("HEAD"),
+                    to = @At(value = "INVOKE", target = "Lnet/minecraft/network/Packet;processPacket(Lnet/minecraft/network/INetHandler;)V")
+            ),
+            at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/network/Packet;processPacket(Lnet/minecraft/network/INetHandler;)V"
-            )
+            ),
+            cancellable = true
     )
-    private void channelRead0(ChannelHandlerContext p_channelRead0_1_, Packet<?> packet, CallbackInfo info) {
+    private void channelRead0(ChannelHandlerContext context, Packet<?> packet, CallbackInfo info) {
         ClientPacketRecieveEvent event = new ClientPacketRecieveEvent(packet);
         AsunaMod.EVENT_MANAGER.invokeEvent(event);
-        if (event.isCancelled()) {
-            info.cancel();
-        }
+       if (event.isCancelled()) info.cancel();
     }
 }
